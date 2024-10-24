d6307be618ebdc9782ccf2ae7d1e1f88db2bbaad
ashishshrm12 <ashishsharmamngr@gmail.com>
2024-10-24 12:55:55 +0530

    sharma

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        aryan
+        sharma
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
